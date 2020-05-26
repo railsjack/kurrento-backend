b1ab69c864d917070e552c2745b4113ef8514926
Vitaly Kroivets <vitaly.kroivets@outlook.com>
2020-05-26 23:31:45 +0300

    Changed the socket url of the server

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const config  =  {
     as_uri: 'https://localhost:3000/',
-    ws_uri:'ws://192.168.0.3:8888/kurento',
+    ws_uri:'ws://35.188.54.111:8888/kurento',
     server_port:3000
 };
 module.exports = config;
