7a3ff9691f9b2bc3d2c301e215219ed08b611deb
Vitaly Kroivets <vitaly.kroivets@outlook.com>
2020-06-06 01:34:12 +0300

    changed socket url

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const config = {
-    // as_uri: 'https://192.168.0.150:3000',
+    as_uri: 'https://video.zuluvideo.com/',
     // ws_uri:'ws://video.zuluvideo.com:8888/kurento',
     ws_uri:'ws://35.188.54.111:8888/kurento',
     server_port: 3000,
