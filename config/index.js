8e1fee5f451205ab355cd746a78603ce3baea040
Vitaly Kroivets <vitaly.kroivets@outlook.com>
2020-06-06 00:29:46 +0300

    Changed the urls to server's

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 const config = {
-    as_uri: 'https://video.zuluvideo.com/',
-    ws_uri:'ws://35.188.54.111:8888/kurento',
+    // as_uri: 'https://192.168.0.150:3000',
+    ws_uri:'ws://video.zuluvideo.com:8888/kurento',
+    // ws_uri:'ws://192.168.0.3:8888/kurento',
     server_port: 3000,
     mode: 'server',
     env: {
